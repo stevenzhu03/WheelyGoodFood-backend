2de47bfe68a2511f649d66f03636100f0e2f4c9d
Steven Zhu <stevenzhu03@gmail.com>
2020-02-28 10:53:50 -0500

    added controllers

--- Front End/src/index.js
+++ Front End/src/index.js
@@ -6,9 +6,33 @@ window.addEventListener("DOMContentLoaded", e =>{
         switch(true) {
             case(e.target.id === "spin_button"):
                 e.preventDefault()
+                let form = e.target.parentNode
+                postToBackEnd(form)
                 break;
         }
 
     })//end of click listener
 
+    
 })//end of DOMcontentloaded
+
+function postToBackEnd(form){
+
+    let location = form.location.value
+    let type = form.type.value
+    let price = form.price.value
+
+    let newSearch = {location, type, price}
+
+    fetch('http://localhost:3000/yelp', {
+        method: "POST",
+        headers: {
+            'content-type': 'application/json',
+            accept: 'application/json'
+        },
+        body: JSON.stringify(newSearch)
+    })
+    .then(resp => resp.json())
+    .then(search => console.log)
+    
+}
