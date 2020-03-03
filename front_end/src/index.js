2263373e0808576b26ce33f79b2b5e7ff5ffcc6b
jhuang429 <jhuang9240@gmail.com>
2020-03-03 15:41:24 -0500

    google maps

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -144,3 +144,43 @@ function toggleWheelAndCard(){
     card.hidden = !card.hidden
     wheel.hidden = !wheel.hidden
 }
+
+
+
+//google maps
+var map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.706001, lng: -74.008804},
+    zoom: 15
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('LETS EAT HERE');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
