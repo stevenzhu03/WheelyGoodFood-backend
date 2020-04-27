67a706952d87a00795a2ae171164e1c91ffe7852
jhuang429 <jhuang9240@gmail.com>
2020-04-27 19:39:12 -0400

    gitignore

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,34 +1,34 @@
-window.addEventListener("DOMContentLoaded", e =>{
+window.addEventListener("DOMContentLoaded", e => {
     let form = document.getElementById("form")
 
     fetch("http://localhost:3000/spins/recent").then(resp => resp.json()).then(
-        content=>{
+        content => {
             let recent = document.getElementById("recent-restaurant")
-            content.forEach(restaurant =>{
+            content.forEach(restaurant => {
 
-            li = document.createElement("li")
-            li.innerHTML = `<a href=${restaurant.url} target="_blank">${restaurant.name}</a>`
-            recent.append(li)
-        
+                li = document.createElement("li")
+                li.innerHTML = `<a href=${restaurant.url} target="_blank">${restaurant.name}</a>`
+                recent.append(li)
+
+            })
         })
-    })
 
     fetch("http://localhost:3000/spins/popular").then(resp => resp.json()).then(
-        content=>{
+        content => {
             let popular = document.getElementById("popular-restaurant")
-            content.forEach(restaurant =>{
+            content.forEach(restaurant => {
 
-            li = document.createElement("li")
-            li.innerHTML = `<a href=${restaurant.url} target="_blank">${restaurant.name}</a> Hits: ${restaurant.spun}`
-            popular.append(li)
-        
+                li = document.createElement("li")
+                li.innerHTML = `<a href=${restaurant.url} target="_blank">${restaurant.name}</a> Hits: ${restaurant.spun}`
+                popular.append(li)
+
+            })
         })
-    })
-    
-    
-    document.addEventListener("click", e =>{
-        switch(true) {
-            case(e.target.id === "yelp_search"):
+
+
+    document.addEventListener("click", e => {
+        switch (true) {
+            case (e.target.id === "yelp_search"):
                 e.preventDefault()
                 let form = document.getElementById("form")
                 postToBackEnd(form)
@@ -37,16 +37,16 @@ window.addEventListener("DOMContentLoaded", e =>{
         }
     })//end of click listener
 
-    
+
 })//end of DOMcontentloaded
 
-function postToBackEnd(form){
-    
+function postToBackEnd(form) {
+
     let location = form.location.value
     let type = form.type.value
     let price = form.price.value
 
-    let newSearch = {location, type, price}
+    let newSearch = { location, type, price }
 
     fetch('http://localhost:3000/yelp', {
         method: "POST",
@@ -56,45 +56,45 @@ function postToBackEnd(form){
         },
         body: JSON.stringify(newSearch)
     })
-    .then((response) => response.json())
-    .then(search => {
-        // console.log(search)
-        for (i=0; i < search.length; i++){
-            //check if restaurant name is over 15 chars
-            if (search[i].name.length > 15){
-                let arr = search[i].name.split(" ")
-                resultsArray[i]["text"] = arr.slice(0, 2).join(" ")
-            }
-            else {resultsArray[i]["text"] = search[i].name}
+        .then((response) => response.json())
+        .then(search => {
+            // console.log(search)
+            for (i = 0; i < search.length; i++) {
+                //check if restaurant name is over 15 chars
+                if (search[i].name.length > 15) {
+                    let arr = search[i].name.split(" ")
+                    resultsArray[i]["text"] = arr.slice(0, 2).join(" ")
+                }
+                else { resultsArray[i]["text"] = search[i].name }
 
-            resultsArray[i]["id"] = search[i].id
-        }
-        let wheel = document.getElementById("wheel")
-        wheel.hidden = false
-        theWheel = new Winwheel({
-            'numSegments'  : 8,     // Specify number of segments.
-            'outerRadius'  : 212,   // Set outer radius so wheel fits inside the background.
-            'textFontSize' : 18,    // Set font size as desired.
-            'segments'     :        // Define segments including colour and text.
-            resultsArray,
-            'animation' :           // Specify the animation to use.
-            {
-                'type'     : 'spinToStop',
-                'duration' : 5,     // Duration in seconds.
-                'spins'    : 8,     // Number of complete spins.
-                'callbackFinished' : alertPrize
+                resultsArray[i]["id"] = search[i].id
             }
-        });
-        
-        
-    })
-    
-    
+            let wheel = document.getElementById("wheel")
+            wheel.hidden = false
+            theWheel = new Winwheel({
+                'numSegments': 8,     // Specify number of segments.
+                'outerRadius': 212,   // Set outer radius so wheel fits inside the background.
+                'textFontSize': 18,    // Set font size as desired.
+                'segments':        // Define segments including colour and text.
+                    resultsArray,
+                'animation':           // Specify the animation to use.
+                {
+                    'type': 'spinToStop',
+                    'duration': 5,     // Duration in seconds.
+                    'spins': 8,     // Number of complete spins.
+                    'callbackFinished': alertPrize
+                }
+            });
+
+
+        })
+
+
 }//end of postToBackEnd
 
-function searchBusiness(indicatedSegment){
+function searchBusiness(indicatedSegment) {
     let businessId = indicatedSegment.id
-    let searchSelection = {id: businessId}
+    let searchSelection = { id: businessId }
     fetch('http://localhost:3000/yelp/business', {
         method: "POST",
         headers: {
@@ -103,23 +103,23 @@ function searchBusiness(indicatedSegment){
         },
         body: JSON.stringify(searchSelection)
     })
-    .then(response => response.json())
-    .then(restaurant => {
-        let wheel = document.getElementById("wheel")
-        wheel.hidden = true
+        .then(response => response.json())
+        .then(restaurant => {
+            let wheel = document.getElementById("wheel")
+            wheel.hidden = true
 
-                console.log(restaurant)
-                let card = document.getElementById('card')
-                card.hidden= false
-                
-                let hours ={1: "Closed", 2: "Closed", 3: "Closed", 4: "Closed", 5: "Closed", 6: "Closed", 7: "Closed"}
-                restaurant.business_info.hours[0].open.forEach(day=>{
-                    hours[day.day] = `${FormatTime(day.start)}-${FormatTime(day.end)}`
-                })
-    
-                
+            console.log(restaurant)
+            let card = document.getElementById('card')
+            card.hidden = false
 
-                card.innerHTML=`
+            let hours = { 1: "Closed", 2: "Closed", 3: "Closed", 4: "Closed", 5: "Closed", 6: "Closed", 7: "Closed" }
+            restaurant.business_info.hours[0].open.forEach(day => {
+                hours[day.day] = `${FormatTime(day.start)}-${FormatTime(day.end)}`
+            })
+
+
+
+            card.innerHTML = `
                 <div id="card-title">
                     <h2><a href=${restaurant.business_info.url} target="_blank">${restaurant.business_info.name}</a></h2>
                 </div>
@@ -133,7 +133,7 @@ function searchBusiness(indicatedSegment){
                     <div id="card-details">
                     
                         <p> <span> Rating: </span> <img src="./assets/${restaurant.business_info.rating}.png" <span>&ensp;(${restaurant.business_info.review_count})</span></p>
-                        <p><span>Open Now? </span> ${restaurant.business_info.hours[0].is_open_now? "Open" : "Closed"}</p>
+                        <p><span>Open Now? </span> ${restaurant.business_info.hours[0].is_open_now ? "Open" : "Closed"}</p>
                         <p> <span>Phone Number: </span>${formatPhoneNumber(restaurant.business_info.phone)} </p>
                         <p> <span>Address:</span> ${restaurant.business_info.location.display_address[0]}, ${restaurant.business_info.location.display_address[1]}</p>
 
@@ -154,11 +154,11 @@ function searchBusiness(indicatedSegment){
 
                 </div>
                 `
-    
 
 
-                let map = document.getElementById("map")
-                map.innerHTML = 
+
+            let map = document.getElementById("map")
+            map.innerHTML =
                 `
                 <iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=${restaurant.business_info.location.display_address}&key=AIzaSyBLJO5Se7usAdXjNZ4F6rwwV9K5xgyZNJg" allowfullscreen></iframe>
                 `
@@ -166,7 +166,7 @@ function searchBusiness(indicatedSegment){
 
             //load thumbnails
             let thumbnails = document.getElementById('thumbnails');
-            restaurant.business_info.photos.forEach(photo=>{
+            restaurant.business_info.photos.forEach(photo => {
                 let pic = document.createElement("img")
                 pic.src = photo
                 pic.height = "100"
@@ -176,28 +176,28 @@ function searchBusiness(indicatedSegment){
 
             resetWheel()
 
-    })
+        })
 }//end of search business
 
 //helper functions
 
-function toggleWheelAndCard(){
+function toggleWheelAndCard() {
     let card = document.getElementById("card")
     let wheel = document.getElementById("wheel")
     card.hidden = !card.hidden
     wheel.hidden = !wheel.hidden
 }
 
-function readdForm(){
+function readdForm() {
     location.reload()
 }
 
-FormatTime = function (fourDigitTime){
-    var hours24 = parseInt(fourDigitTime.substring(0,2));
+FormatTime = function (fourDigitTime) {
+    var hours24 = parseInt(fourDigitTime.substring(0, 2));
     var hours = ((hours24 + 11) % 12) + 1;
     var amPm = hours24 > 11 ? 'pm' : 'am';
     var minutes = fourDigitTime.substring(2);
-    
+
     return hours + ':' + minutes + amPm;
 };
 
@@ -206,9 +206,9 @@ function formatPhoneNumber(phoneNumberString) {
     var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
     var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/)
     if (match) {
-      var intlCode = (match[1] ? '+1 ' : '')
-      return ['(', match[2], ') ', match[3], '-', match[4]].join('')
+        var intlCode = (match[1] ? '+1 ' : '')
+        return ['(', match[2], ') ', match[3], '-', match[4]].join('')
     }
     return null
-  }
+}
 
